56f99239128301e613175bbfc789291e3fe068ab
ShineHtetAung99 <shinehtetaung14499@gmail.com>
2025-08-27 15:35:43 +0630

    add-vite-js-design-link-in-layout-file

--- backend/resources/views/components/layout.blade.php
+++ backend/resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('images/ni-logo-only.jpg') }}">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50">
     <x-navbar/>
