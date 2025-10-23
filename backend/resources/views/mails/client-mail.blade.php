69a5d1c50e58979ff24755e4eb696de429282a64
ShineHtetAung99 <shinehtetaung14499@gmail.com>
2025-10-23 16:25:55 +0630

    fix-client-mail-template-text-issue-at-android

--- backend/resources/views/mails/client-mail.blade.php
+++ backend/resources/views/mails/client-mail.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="jp">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,22 +26,29 @@
     <p style="color: black;">お客様情報</p>
     <table style="width:800px; padding-left: 20px;">
         <tr>
-            <td style="width: 35%; font-weight:bold">会社名</td>
+            <td style="width: 150px; font-weight:bold">会社名</td>
             <td>:</td>
             <td>{{$contact['company_name']}}</td>
         </tr>
         <tr>
-            <td style="width: 35%; font-weight:bold">お名前</td>
+            <td style="width: 150px; font-weight:bold">お名前</td>
             <td>:</td>
             <td>{{$contact['name']}}</td>
         </tr>
         <tr>
-            <td style="width: 35%; font-weight:bold">メールアドレス</td>
+            <td style="width: 150px; font-weight:bold">メールアドレス</td>
             <td>:</td>
-            <td>{{$contact['email']}}</td>
+            <td style="
+                max-width:300px;
+                word-break:break-word;
+                overflow-wrap:anywhere;
+                white-space:normal;
+            ">
+                {{$contact['email']}}
+            </td>
         </tr>
         <tr>
-            <td style="width: 35%; font-weight:bold">電話番号</td>
+            <td style="width: 150px; font-weight:bold">電話番号</td>
             <td>:</td>
             <td>{{$contact['phone']}}</td>
         </tr>
@@ -61,9 +68,17 @@
                     '12' => 'その他',
                 ];
             @endphp
-            <td style="width: 35%; font-weight:bold">ご相談サービス</td>
+            <td style="width: 150px; font-weight:bold">ご相談サービス</td>
             <td>:</td>
-            <td>{{$services[$contact['interest']]}}</td>
+            <td
+                style="
+                max-width:300px;
+                word-break:break-word;
+                overflow-wrap:anywhere;
+                white-space:normal;
+            ">
+                {{$services[$contact['interest']]}}
+            </td>
         </tr>
         <tr>
             @php
@@ -74,7 +89,7 @@
                     '4' => 'その他',
                 ];
             @endphp
-            <td style="width: 35%; font-weight:bold">希望方式</td>
+            <td style="width: 150px; font-weight:bold">希望方式</td>
             <td>:</td>
             <td>{{$contact['hope'] ? $hope[$contact['hope']] : ''}}</td>
         </tr>
@@ -97,7 +112,7 @@
                     '14' => '応相談',
                 ];
             @endphp
-            <td style="width: 35%; font-weight:bold">ご予算</td>
+            <td style="width: 150px; font-weight:bold">ご予算</td>
             <td>:</td>
             <td>{{$contact['budget_jp'] ? $budget[$contact['budget_jp']] : ''}}</td>
         </tr>
@@ -110,12 +125,12 @@
                     '4' => '営業',
                 ];
             @endphp
-            <td style="width: 35%; font-weight:bold">知ったきっかけ</td>
+            <td style="width: 150px; font-weight:bold">知ったきっかけ</td>
             <td>:</td>
             <td>{{$known[$contact['known']]}}</td>
         </tr>
         <tr>
-            <td style="width: 35%; font-weight:bold">詳細内容</td>
+            <td style="width: 150px; font-weight:bold">詳細内容</td>
             <td>:</td>
             <td>{{$contact['message']}}</td>
         </tr>
@@ -125,29 +140,29 @@
         <p>
             2営業日以内に返信がない場合は、恐れ入りますが
             <span class="desktop-link">
-                <a href="https://mail.google.com/mail/?view=cm&to=info@next-innovations.ltd">info@next-innovations.ltd</a>
+                <a href="https://mail.google.com/mail/?view=cm&to=info@next-innovations.ltd" target="_blank" style="color:#1a73e8; text-decoration:none;">info@next-innovations.ltd</a>
             </span>
             <span class="mobile-link">
-                <a href="mailto:info@next-innovations.ltd" target="__blank">info@next-innovations.ltd</a>
+                <a href="mailto:info@next-innovations.ltd" target="_blank" style="color:#1a73e8; text-decoration:none;">info@next-innovations.ltd</a>
             </span>
             まで直接ご連絡ください。
         </p>
         <p>サービスの詳細はこちらからもご覧いただけます：</p>
         <p>
             ・
-            <a href="https://jp.next-innovations.ltd/eor">EORサービスページ</a>
+            <a href="https://jp.next-innovations.ltd/eor" target="_blank" style="color:#1a73e8; text-decoration:none;">EORサービスページ</a>
         </p>
         <p>
             ・
-            <a href="https://jp.next-innovations.ltd/dx-support">DX支援サービスページ</a>
+            <a href="https://jp.next-innovations.ltd/dx-support" target="_blank" style="color:#1a73e8; text-decoration:none;">DX支援サービスページ</a>
         </p>
     </div>
     <p style="color: black;">Next Innovations</p>
-    <a href="https://jp.next-innovations.ltd">https://jp.next-innovations.ltd</a>
+    <a href="https://jp.next-innovations.ltd" target="_blank" style="color:#1a73e8; text-decoration:none;">https://jp.next-innovations.ltd</a>
     <div style="padding-left: 20px;">
         <p>さらに弊社を知っていただけるコンテンツもぜひご覧ください：</p>
-        <p><a href="https://demo-site.next-innovations.ltd/ni-blog">会社公式ブログはこちら</a></p>
-        <a href="https://note.com/yuta_myanmar_eor">代表ブログはこちら</a>
+        <p><a href="https://demo-site.next-innovations.ltd/ni-blog" target="_blank" style="color:#1a73e8; text-decoration:none;">会社公式ブログはこちら</a></p>
+        <a href="https://note.com/yuta_myanmar_eor" target="_blank" style="color:#1a73e8; text-decoration:none;">代表ブログはこちら</a>
     </div>
 
 </body>
